f408cb6d9bebc88f15440abe4779a711bffb709c
Garry Gajultos <gajultos.garry123@gmail.com>
2024-11-26 07:53:49 +0800

    test

--- employee_area/employee_dashboard.php
+++ employee_area/employee_dashboard.php
@@ -142,12 +142,6 @@ if ($result_absent->num_rows > 0) {
 
 </html>
 
-<!-- Add Bootstrap CSS -->
-<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet">
-
-<!-- Add Bootstrap JS and Popper.js for Modal functionality -->
-<script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.10.2/dist/umd/popper.min.js"></script>
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.min.js"></script>
 
 <script>
     // Create the real calendar
